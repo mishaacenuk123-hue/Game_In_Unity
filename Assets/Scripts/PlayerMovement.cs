eeb248defb88f4224b1f851ef6ad58ddb96b1db7
mishaacenuk123-hue <mishaacenuk123@gmail.com>
2025-12-10 23:17:18 +0200

    Version. 0.0.3
    -Add textures
    -Add animation
    -Add Character
    -Fix infinity jumps
    bug fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,18 +5,45 @@ public class PlayerMovement : MonoBehaviour
 {
     [SerializeField] private float speed;
     private Rigidbody2D body;
+    private Animator anim;
+    private bool grounded;
     private void Awake()
-
+    //Отримання посилання 
     {
         body = GetComponent<Rigidbody2D>();
+        anim= GetComponent<Animator>();
     }
     private void Update()
+    {     //Присвоєння горизонтального вводу змінній
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.linearVelocity = new Vector2(horizontalInput * speed, body.linearVelocity.y);
+        //Поворот персонажа в залежності від напрямку руху
+        if (horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if (horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            Jump();
+            
+        //Анімація бігу 
+        anim.SetBool("run", horizontalInput != 0);
+        anim.SetBool("grounded", grounded);
+
+
+    }
+
+    private void Jump()
     {
-        body.linearVelocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.linearVelocity.y);
-        if (Input.GetKeyDown(KeyCode.Space))
-        {
-            body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
-        }
+        body.linearVelocity = new Vector2(body.linearVelocity.x, speed);
+        anim.SetTrigger("jump");
+        grounded = false;
+    }
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Ground")
+        grounded = true;
+        
     }
 
 }
