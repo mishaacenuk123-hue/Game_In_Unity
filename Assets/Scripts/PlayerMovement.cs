f9b4b7ed231d36bb048d4e2fb083a8ba0b3d7e90
mishaacenuk123-hue <mishaacenuk123@gmail.com>
2025-12-11 23:40:46 +0200

    Version. 0.0.5
    +Add attacks
    +New animation
    bug fix

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -76,14 +76,7 @@ else
             wallJumpCooldown = 0;
            
         }
-    }
-
-            private void OnCollisionEnter2D(Collision2D collision)
-            {
-
-
-            }
-        
+    }   
     private bool isGrounded()
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
@@ -93,5 +86,9 @@ else
     {
         RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
         return raycastHit.collider != null;
+    }
+    public bool canAttack()
+    {
+        return horizontalInput == 0 && isGrounded()&& !onWall();
     }
 }
