b766cb538b4811726c81dd1257d7dd359867361e
mishaacenuk123-hue <mishaacenuk123@gmail.com>
2025-12-15 18:26:54 +0200

    Vesion 1.0.0
    Add- New room
    Add -sounds
    Add-Paused
    Add-Traps
    Add-Enemy
    More and more

--- Assets/Scripts/Health/Healthbar.cs
+++ Assets/Scripts/Health/Healthbar.cs
@@ -1,18 +1,16 @@
-using UnityEngine.UI;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Healthbar : MonoBehaviour
 {
     [SerializeField] private Health playerHealth;
     [SerializeField] private Image totalhealthBar;
     [SerializeField] private Image currenthealthBar;
-    
 
     private void Start()
     {
         totalhealthBar.fillAmount = playerHealth.currentHealth / 10;
     }
-
     private void Update()
     {
         currenthealthBar.fillAmount = playerHealth.currentHealth / 10;
